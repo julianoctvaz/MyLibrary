a81b0af0062799e93f11161fefb4252997001cb6
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-02-15 11:56:02 -0300

    Creates fetchBooks function simulating an data bank out of application

--- MyLibrary/BooksViewModel.swift
+++ MyLibrary/BooksViewModel.swift
@@ -17,4 +17,29 @@ class BooksViewModel: ObservableObject { //Quando você adota esse protocolo em 
     //Em um aplicativo, existem muitos cenários em que eles são úteis. Os campos do formulário que não estão preenchidos são apenas um deles.
     @Published var books = [Book]()
    // agora, vamos falar sobre @Published. Já sabemos que ObservableObject usa propriedades que têm o wrapper de propriedade @Published, mas há mais. O wrapper de propriedade @Published essencialmente transforma a propriedade em um editor. Isso significa que você pode assinar (subscribes) a propriedade e responder às alterações que o editor (Published) emite. Isso é o que faremos com @ObservedObject, mais tarde.
+
+
+/*
+ A próxima etapa que daremos é buscar os dados do livro na BooksViewModel classe.
+ 
+ Conforme discutimos, o modelo de exibição é (vagamente) responsável por obter dados de uma fonte de dados (externa). Este não é o propósito do modelo de visualização no sentido mais estrito. Como não temos um controlador de banco de dados ou camada de persistência separada, é o suficiente por enquanto.
+ */
+
+    func fetchBooks(){
+    URLSession.shared.dataTaskPublisher(for: url)
+        .map { $0.data } //taking the output of the publisher
+        .decode(type: [Book].self, decoder: JSONDecoder())//decoding the data value, with the JSONDecoder decoder, and create an ARRAY of Book objects from it.
+        .replaceError(with: []) //itsessentially replace any errors with an empty array; the best-case approach, por enquanto, tudo bem, mas em seus próprios aplicativos você vai querer responder aos erros de outra maneira. Por exemplo, você pode escolher rejeitar códigos de status HTTP diferentes de 200.
+        .eraseToAnyPublisher() // topico mais complexo, so assuma que isso mudará o tipo de editor de algo complexo para o mais simples AnyPublisher<[Book], Never>
+        .receive(on: DispatchQueue.main) //receber suas atualizações no thread principal, para "poder atualizar as visualizacoes do fio de execucao principal""
+        .assign(to: &$books) //estamos dizendo ao editor - você sabe, o código acima dele - para republicar os valores que ele emite e atribuí-los à books propriedade. Pense nisso como um macaco que repete tudo o que outro macaco diz. E esses &$ significa que o mcador do To: é um inout, e precisa tb ser passado por referencia, e nao por copia de valores.
+        
+       // Mais informacoes https://learnappmaking.com/urlsession-swift-networking-how-to/
+        //A partir do link acima a um uso generico de urlsession, sem que seja com publisher.
+        //NAo usamos um shared.dataTask qualquer, em vez de um manipulador de conclusão (completion handler) para a resposta, este código cria um editor, um  PUBLISHER
+
+    }
+/*
+De uma visão panorâmica, este é o código que usa a estrutura Combine para obter um arquivo JSON baseado na web, transformá-lo em uma matriz de Bookobjetos e atribuí-lo à propriedade books de BooksViewModel. É uma “cadeia” de operadores Combine, que usa o padrão builder que você já conhece do SwiftUI.
+*/
 }
